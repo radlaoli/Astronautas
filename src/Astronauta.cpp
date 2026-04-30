a56fbc9f6f7ab2e1f3b750bcadfbf7a2ffed83e1
radla <radlaoliveira5@gmail.com>
2026-04-30 14:34:16 -0300

    implementaçao do Astronauta.cpp e mudança no nome do metodo exibirEstado para exibirInfo

---
+++ src/Astronauta.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include "../include/Astronauta.hpp"
+
+using namespace std;
+
+Astronauta::Astronauta(string cpf, string nome, int idade)
+{
+    this->CPF = cpf;
+    this->nome = nome;
+    this->idade = idade;
+    this->disponibilidade = true; // disponivel
+    this->estado = true;          // vivo
+}
+
+Astronauta::~Astronauta() {}
+
+void Astronauta::exibirInfo()
+{
+    cout << "CPF: " << CPF << " | Nome: " << nome << " | Idade: " << idade << endl;
+    if (estado) // vivo
+    {
+        cout << "Estado: Vivo | ";
+    }
+    else
+    {
+        cout << "Estado: Morto | ";
+    }
+
+    if (disponibilidade)
+    { // disponivel
+        cout << "Disponibilidade: Disponivel" << endl;
+    }
+    else
+    {
+        cout << "Disponibilidade: Indisponivel" << endl;
+    }
+}
