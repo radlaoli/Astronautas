1971aaef792e9b91f2c4207d412c9e713d61b95a
radla <radlaoliveira5@gmail.com>
2026-04-30 14:03:39 -0300

    esqueleto da classe voo

---
+++ include/Voo.hpp
@@ -0,0 +1,25 @@
+#ifndef VOO_HPP
+#define VOO_HPP
+
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector;
+class Voo
+{
+public:
+    int codigo;
+    vector<string> CPFs_Astronautas;
+    enum estado
+    {
+        planejado,
+        emCurso,
+        finalizadoSucesso,
+        finalizadoExplosao
+    };
+
+    Voo(int codigo);
+    ~Voo();
+};
+#endif
